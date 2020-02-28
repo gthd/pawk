43683948d28f0de3a21ad3114c0c815a131df753
unknown <gtheodorou035@gmail.com>
2020-02-28 10:46:36 +0200

    fix offset

--- demo.go
+++ demo.go
@@ -16,6 +16,7 @@ func check(e error) {
 func main() {
 	var end int
 	var bytes_to_read int
+	var data string
 	o := int64(0)
 	bytes_to_read = 0
 	end = 0
@@ -32,23 +33,31 @@ func main() {
 	fmt.Printf("the file size is: %d\n", size)
 	default_size := int(size/int64(number_of_threads))
 	for thread :=0; thread < number_of_threads; thread++ {
-		fmt.Println(bytes_to_read - end)
-		bytes_to_read = default_size + (bytes_to_read - end)
+		//fmt.Println(bytes_to_read - end)
+		if thread == number_of_threads - 1 {
+				bytes_to_read = default_size + (bytes_to_read - end) + 2
+		} else {
+			bytes_to_read = default_size + (bytes_to_read - end)
+		}
 		b := make([]byte, bytes_to_read) //the byte length that gets handled by every thread
 		n, err := f.Read(b)
 		check(err)
-		fmt.Printf("%d bytes @ %d\n", n, o)
-		fmt.Printf("%v\n", string(b[:n]))
+		fmt.Printf("\n\n%d bytes @ %d\n", n, o)
+		//fmt.Printf("%v\n", string(b[:n]))
 		for i :=bytes_to_read-1; i > 0; i-- {
 			if string(b[i]) == "\n" {
 				end = i
 				break
 			}
 		}
-		data := string(b[:end])
-		fmt.Printf("%v \n", data)
-		fmt.Printf("%d \n \n \n",end)
-		o, err = f.Seek(int64(thread*default_size + end),0)
+		if thread > 0 {
+			data = string(b[1:end])
+		} else {
+			data = string(b[:end])
+		}
+		fmt.Printf("%v\n", data)
+		//fmt.Printf("%d \n \n \n",end)
+		o, err = f.Seek(o+int64(end),0)
 		check(err)
 	}
 }
