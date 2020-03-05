ed9a988239e3bb122515f283d2dc2be407939966
unknown <gtheodorou035@gmail.com>
2020-03-05 22:53:11 +0200

    use all cores

--- demo.go
+++ demo.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
-	"flag"
-	"strconv"
+	"runtime"
+	"time"
 )
 
 func check(e error) {
@@ -14,26 +14,21 @@ func check(e error) {
 }
 
 func main() {
-	var end int
-	var bytes_to_read int
+	start := time.Now()
+	number_of_threads := runtime.GOMAXPROCS(0)
 	var data string
 	o := int64(0)
-	bytes_to_read = 0
-	end = 0
-	flag.Parse()
-	s1 := flag.Arg(0)
-	number_of_threads, err := strconv.Atoi(s1)
+	bytes_to_read := 0
+	end := 0
+	f, err := os.Open("demo.txt")
 	check(err)
-	file := flag.Arg(1)
-	f, err := os.Open(file)
-	check(err)
+	defer f.Close()
 	file_size, err := f.Stat()
 	check(err)
 	size:= file_size.Size()
 	fmt.Printf("the file size is: %d\n", size)
 	default_size := int(size/int64(number_of_threads))
 	for thread :=0; thread < number_of_threads; thread++ {
-		//fmt.Println(bytes_to_read - end)
 		if thread == number_of_threads - 1 {
 				bytes_to_read = default_size + (bytes_to_read - end) + 2
 		} else {
@@ -43,7 +38,6 @@ func main() {
 		n, err := f.Read(b)
 		check(err)
 		fmt.Printf("\n\n%d bytes @ %d\n", n, o)
-		//fmt.Printf("%v\n", string(b[:n]))
 		for i :=bytes_to_read-1; i > 0; i-- {
 			if string(b[i]) == "\n" {
 				end = i
@@ -56,8 +50,9 @@ func main() {
 			data = string(b[:end])
 		}
 		fmt.Printf("%v\n", data)
-		//fmt.Printf("%d \n \n \n",end)
 		o, err = f.Seek(o+int64(end),0)
 		check(err)
 	}
+	elapsed := time.Since(start)
+	fmt.Printf("Time it took %s", elapsed)
 }
