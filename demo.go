9dc6d01f4a6814d8f139eb6e05f08039babc34eb
gthd <gtheodorou035@gmail.com>
2020-03-23 01:09:47 +0200

    solve issue #5

--- demo.go
+++ demo.go
@@ -4,10 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"os"
-	"reflect"
 	"runtime"
 	"strconv"
-	"sync"
 	"time"
 	// "net/http"
 	// _ "net/http/pprof"
@@ -18,8 +16,7 @@ import (
 )
 
 type chunk struct {
-	buffsize int
-	offset   int64
+	buff []byte
 }
 
 func check(e error) {
@@ -94,57 +91,51 @@ func getSize(file *os.File) int {
 	return filesize
 }
 
-func divideFile(filesize int, n int) (int, []chunk) {
-	chunksizes := make([]chunk, n)
-	BufferSize := int(filesize / int(n))
-	for i := 0; i < n; i++ {
-		chunksizes[i].buffsize = BufferSize
-		chunksizes[i].offset = int64(BufferSize * i)
-	}
-	if remainder := filesize % BufferSize; remainder != 0 {
-		c := chunk{buffsize: remainder, offset: int64(n * BufferSize)}
-		n++ //then the last chunk is not processed in parallel, as the go-routines are one more than the cores available
-		chunksizes = append(chunksizes, c)
-	}
-	return n, chunksizes
-}
-
-func endOffset(buffsize int, buffer []byte) int {
-	endingOffset := 0
-	for j := buffsize - 1; j > 0; j-- { //going backward on the last line to find where it starts
-		if string(buffer[j]) == "\n" {
-			endingOffset = j + 1
-			break
+func divideFile(file *os.File, n int) []chunk {
+	chunk := make([]chunk, n)
+	var data string
+	o := int64(0)
+	bytes_to_read := 0
+	end := 0
+	filesize := getSize(file)
+	default_size := int(filesize / int(n))
+	for thread := 0; thread < n; thread++ {
+		if thread == n-1 {
+			bytes_to_read = default_size + (bytes_to_read - end) + 2
+		} else {
+			bytes_to_read = default_size + (bytes_to_read - end)
 		}
-	}
-	return endingOffset
-}
+		b := make([]byte, bytes_to_read) //the byte length that gets handled by every thread
+		textBytes, err := file.Read(b)
+		check(err)
+		_ = textBytes
+		_ = o
+		// fmt.Printf("\n\n%d bytes @ %d\n", textBytes, o)
 
-func startOffset(buffsize int, buffer []byte) int {
-	startingOffset := 0
-	for k := 0; k < buffsize; k++ { //going forward on the first line to find where it ends
-		if string(buffer[k]) == "\n" {
-			startingOffset = k
-			break
+		for i := bytes_to_read - 1; i > 0; i-- {
+			if string(b[i]) == "\n" {
+				end = i
+				break
+			}
 		}
+
+		if thread > 0 {
+			chunk[thread].buff = b[1:end]
+			data = string(b[1:end])
+		} else {
+			chunk[thread].buff = b[:end]
+			data = string(b[:end])
+		}
+		_ = data
+		o, err = file.Seek(o+int64(end), 0)
+		check(err)
 	}
-	return startingOffset
+	return chunk
 }
 
-func stichLines(sm sync.Map, n int) {
-	firstLine, _ := sm.Load("start" + strconv.Itoa(1))
-	fmt.Printf("line is %s \n", firstLine)
-	for i := 1; i < n; i++ {
-		stringResultEnd, _ := sm.Load("end" + strconv.Itoa(i))
-		stringResultStart, _ := sm.Load("start" + strconv.Itoa(i+1))
-		line := stringResultEnd.(string) + stringResultStart.(string)
-		fmt.Printf("line is %s \n", line)
-	}
-}
-
-func goAwk(buffer []byte, startingOffset int, endingOffset int, prog *parser.Program) {
+func goAwk(chunk []byte, prog *parser.Program) {
 	config := &interp.Config{
-		Stdin: bytes.NewReader([]byte(string(buffer[startingOffset:endingOffset]))),
+		Stdin: bytes.NewReader(chunk),
 		Vars:  []string{"OFS", ":"},
 	}
 	_, err := interp.ExecProgram(prog, config)
@@ -157,47 +148,28 @@ func main() {
 	// go func() {
 	// log.Println(http.ListenAndServe("localhost:6060", nil))
 	// }()
-
 	start := time.Now()
 	arg0, n, arg1, commandInFile := receiveArguments()
 	awkCommand := getCommand(commandInFile, arg0)
+	res := make(chan int)
 	// n := runtime.GOMAXPROCS(0)
 	// src := "$2 * $3 > 5 { emp = emp + 1 } END {print emp}"
 	prog, err, varTypes := parser.ParseProgram([]byte(awkCommand), nil)
 	check(err)
 
-	fmt.Println(reflect.TypeOf(prog))
-
 	if len(varTypes) > 1 {
 		panic("Cannot handle awk command that contains local variables")
 	}
 
-	var sm sync.Map
-	res := make(chan int)
 	file := openFile(arg1)
 	defer file.Close()
-	filesize := getSize(file)
-	n, chunksizes := divideFile(filesize, n)
-
+	chunks := divideFile(file, n)
+	fmt.Println("############")
 	for i := 0; i < n; i++ {
-		go func(chunksizes []chunk, i int, r chan<- int) {
-			chunk := chunksizes[i]
-			buffer := make([]byte, chunk.buffsize)
-			_, err := file.ReadAt(buffer, chunk.offset)
-			check(err)
+		go func(chunks []chunk, i int, r chan<- int) {
+			chunk := chunks[i]
 
-			endingOffset := endOffset(chunk.buffsize, buffer)
-			startingOffset := startOffset(chunk.buffsize, buffer)
-
-			// Have to change
-			num := strconv.Itoa(i + 1)
-			str := "start" + num
-			ending := "end" + num
-			sm.Store(str, string(buffer[:startingOffset]))
-			sm.Store(ending, string(buffer[endingOffset:]))
-
-			// fmt.Printf("\nbytestream to string new: %v, %d\n", string(buffer[startingOffset:endingOffset]), i)
-			goAwk(buffer, startingOffset, endingOffset, prog)
+			goAwk(chunk.buff, prog)
 
 			// This local variable replaces the global slice.
 			sum := 0
@@ -209,7 +181,7 @@ func main() {
 			// Channel receives the result from processing each chunk
 			r <- sum
 			// Call the goroutine and pass the parameters of each chunk, the CPU core index and the channel that will receive the results.
-		}(chunksizes, i, res)
+		}(chunks, i, res)
 	}
 
 	sum := 0
@@ -220,8 +192,6 @@ func main() {
 		sum += <-res
 	}
 
-	stichLines(sm, n)
-
 	elapsed := time.Since(start)
-	fmt.Printf("\nTime elapsed for channel sum %s\n", elapsed)
+	fmt.Printf("\nTime elapsed %s\n", elapsed)
 }
